a4bf3a89ba644dec98a6fa1cb573c696f7203117
Rafael <rafaelreinertd@hotmail.com>
2023-05-12 14:00:17 -0300

    Change deck

--- src/assets/decks.js
+++ src/assets/decks.js
@@ -1,12 +1,32 @@
+// const deck = [
+// 	{ question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
+// 	{ question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
+// 	{ question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
+// 	{ question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
+// 	{ question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
+// 	{ question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
+// 	{ question: "Usamos props para __", answer: "Passar diferentes informações para componentes" },
+// 	{ question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+// ];
+
 const deck = [
-	{ question: "O que é JSX?", answer: "Uma extensão da linguagem JavaScript" },
-	{ question: "O React é __", answer: "Uma biblioteca JavaScript para construção de interfaces" },
-	{ question: "Componentes devem iniciar com __", answer: "Letra maiúscula" },
-	{ question: "Podemos colocar __ dentro do JSX", answer: "expressões" },
-	{ question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
-	{ question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
-	{ question: "Usamos props para __", answer: "Passar diferentes informações para componentes" },
-	{ question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
-];
+	{question: "Qual o principal objetivo do método ágil em projetos?", answer: "Entregar um produto de alta qualidade"},
+	{question: "Qual a principal diferença entre o método Waterfall e o método Ágil?", answer: "O método Ágil é mais flexível e iterativo, enquanto o Waterfall é linear e sequencial"},
+	{question: "O que é priorização de tarefas em projetos ágeis?", answer: "A definição da prioridade de cada tarefa do projeto baseado em sua relevância para o objetivo final"},
+	{question: "Qual o objetivo do Manifesto Ágil?", answer: "Descrever os valores e princípios do desenvolvimento de software ágil"},
+	{question: "O que são Squads em projetos ágeis?", answer: "Times multidisciplinares e auto-organizados responsáveis por desenvolver uma funcionalidade do produto"},
+	{question: "O que é Scrum em projetos ágeis?", answer: "Um framework ágil para desenvolvimento de software"},
+	{question: "Quais são os papéis do Scrum?", answer: "Desenvolvedor, Gerente de Projeto e Scrum Master"},
+	{question: "O que é um sprint backlog no Scrum?", answer: "Um documento que contém as atividades e tarefas necessárias para atingir os objetivos do sprint."},
+	{question: "O que é o Kanban?", answer: "Uma técnica de gerenciamento de fluxo de trabalho."},
+	{question: "Qual o objetivo do daily scrum?", answer: "Discutir e resolver problemas e impedimentos que estejam atrapalhando o andamento do sprint."},
+	{question: "Qual o papel do Product Owner no Scrum?", answer: "Manter o backlog do produto atualizado e priorizar as funcionalidades de acordo com as necessidades do cliente."},
+	{question: "Qual é a principal vantagem do uso de Squads em projetos ágeis?", answer: "Maior capacidade de resposta às mudanças"},
+	{question: "Qual é o principal objetivo da retrospectiva no Scrum?", answer: "Avaliar o desempenho da equipe durante a sprint"},
+	{question: "Qual é a principal diferença entre o product backlog e o sprint backlog?", answer: "O product backlog contém todas as tarefas do projeto, enquanto o sprint backlog contém apenas as tarefas da sprint atual"},
+	{question: "Qual é a principal vantagem do uso de ferramentas de gestão de projetos como o Jira ou o Trello?", answer: "Permite o controle de atividades e o monitoramento de prazos"},
+	{question: "", answer: ""},
+	{question: "", answer: ""},
+]
 
 export default deck;
